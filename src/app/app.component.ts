7457b7aac96bf4301b229a4555a405dd294510ce
dainoo <ainoodauda@gmail.com>
2021-01-01 21:10:33 +0000

    RxJs operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, VERSION } from "@angular/core";
 import { from, of } from "rxjs";
+import { map, take } from "rxjs/operators";
 
 @Component({
   selector: "my-app",
@@ -16,20 +17,46 @@ export class AppComponent implements OnInit {
       () => console.log("complete man")
     );
 
-    from([20, 15, 10, 5]).subscribe(
-      item => {
-        console.log(`resulting itme ... ${item}`);
-      },
-      err => {
-        console.error(err);
-      },
-      () => console.info("complete")
-    );
+
+    from([20, 15, 10, 5])
+      .pipe(
+        // tab(item => console.log(`emitted item ... ${item} `)),
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if (item === 0) {
+            throw new Error("zero is detected");
+          }
+          return item;
+        }),
+        take(3)
+      )
+      .subscribe(
+        item => {
+          console.log(`resulting itme ... ${item}`);
+        },
+        err => {
+          console.error(err);
+        },
+        () => console.info("complete")
+      );
+
+
 
     from(["A", "B", "C", "D"]).subscribe(
       next => console.log(next),
       err => console.error,
       () => console.info
     );
+
+
+
+    // of(1, 2, 3, 4, 5)
+    //   .pipe(
+    //     tab(item => console.log(`emitted item ... ${item} `)),
+    //     map(item => item * 3),
+    //     map(item => item - 2)
+    //   )
+    //   .subscribe();
   }
 }
